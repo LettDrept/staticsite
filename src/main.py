e6cfde5ef2542875557ffd9bdc5f9a8c30ce2969
LettDrept <bnbardo@gmail.com>
2025-03-03 08:34:42 +0100

    D: textnode updated

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType         # Imports textnode classes
+
+def main():
+    node = TextNode(This is some anchor text, link, https://www.boot.dev)
+    print(node)
+
+main()      # need to call main to execute
---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+from enum import Enum               # Allows for use of Enum-type variables
+
+class TextType(Enum):               # Establishes the standard types of text for this site
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+    
+class TextNode():
+    def __init__(self, text, text_type, url=None):          # Url defaults to None if nothing is passed in
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eg__(self, other):                    # For comparing two TextNode objects
+        return (                                # Returns True if all properties are alike
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__():
+        return(f"TextNode({self.text}, {self.text_type.value}, {self.url})")
